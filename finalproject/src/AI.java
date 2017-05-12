c541a76064ffb12d0525447440006305c5e2ffc3
1833033 <1833033@00-46249.pusd.dom>
2017-05-12 14:57:48 -0700

    remove implements actor

--- finalproject/src/AI.java
+++ finalproject/src/AI.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class AI 
+public class AI implements Actor
 {
 	private ArrayList<Objective> objectiveList;
 	private ArrayList<Bot> botList;
@@ -15,6 +15,13 @@ public class AI
 		for(Objective obj: objectives)
 		{
 			objectiveList.add(obj);
+		}
+	}
+	public void act(Position p1, Position p2, int p3)
+	{
+		for(Bot bot: botList)
+		{
+			bot.act();
 		}
 	}
 }
