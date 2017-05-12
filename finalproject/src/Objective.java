5b615898111525343cf1bc2686046d8b7677e5ae
shayankal <shayankal@live.com>
2017-05-12 14:48:34 -0700

    added parameters for state and position

--- finalproject/src/Objective.java
+++ finalproject/src/Objective.java
@@ -1,5 +1,15 @@
 
 public class Objective 
 {
+	private boolean state; 
+	private Position pos;
+	private final int Xsize = 50;
+	private final int Ysize = 50;
+	
+	public Objective(boolean s, Position position)
+	{
+		state = s;
+		pos = position;
+	}
 
 }
