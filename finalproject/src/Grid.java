507a46216d41e08648e5b8b01cd419e0b51c356a
1833033 <1833033@00-46270.pusd.dom>
2017-05-15 15:18:35 -0700

    updated instantiate() method

--- finalproject/src/Grid.java
+++ finalproject/src/Grid.java
@@ -4,6 +4,7 @@ public class Grid
 	private int xSize;
 	private int ySize;
 	private ArrayList<Actor> actors;
+	private ArrayList<Objective> objectives;
 	private UI gooey;
 	private final int timeLimit;
 	public Grid(int xsize, int ysize)
@@ -15,9 +16,9 @@ public class Grid
 	}
 	public void repaint(Position mouseClickedAt)
 	{
-		if(isValid(mouseClickedAt))
+		if(isInBounds(mouseClickedAt))
 		{
-			Sniper.
+			//Sniper.
 		}
 	}
 	public void repaint(Position mouseClickedAt, Position secondMouseLoc, int keypressed)
@@ -30,7 +31,7 @@ public class Grid
 	}
 	private Bot shoot(Position pos)
 	{
-		return new Bot(new Position(5, 6));
+		return new Bot(new Position(5, 6), 7);
 	}
 	private void SniperMove(Position newLoc)
 	{
@@ -40,11 +41,26 @@ public class Grid
 	{
 		
 	}
+	
 	public void instantiate()
 	{
 		AI ai=new AI(17, this);
-		
+		//Maze maze=new maze();
+		Objective obj1= new Objective(new Position(50, 50));
+		Objective obj2=new Objective(new Position(8500, 50));
+		Objective obj3=new Objective(new Position(8000, 50));
+		Objective obj4=new Objective(new Position(8500, 8000));
+		Spy spy=new Spy();
+		Sniper sniper=new Sniper(new Position(5000, 5000));
+		actors.add(ai);
+		objectives.add(obj1);
+		objectives.add(obj2);
+		objectives.add(obj3);
+		objectives.add(obj4);
+		actors.add(spy);
+		actors.add(sniper);
 	}
+	
 	public boolean isInBounds(Position pos)
 	{
 		return true;
